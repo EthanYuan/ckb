cfa8f6347408dfe3bbc999abba9fa1c07ecb0780
EthanYuan <yuanchengxing@hotmail.com>
2024-08-26 06:58:01 +0800

    add poll_rgbpp_unlock

--- aggregator/aggregator-main/src/branch_to_rgbpp/mod.rs
+++ aggregator/aggregator-main/src/branch_to_rgbpp/mod.rs
@@ -84,6 +84,30 @@ impl Aggregator {
         }
     }
 
+    pub fn poll_rgbpp_unlock(&self, stop_rx: Receiver<()>) {
+        info!("RGB++ Unlock service started ...");
+
+        let poll_interval = self.poll_interval;
+
+        loop {
+            match stop_rx.try_recv() {
+                Ok(_) => {
+                    info!("Aggregator received exit signal, stopped");
+                    break;
+                }
+                Err(crossbeam_channel::TryRecvError::Empty) => {
+                    // No exit signal, continue execution
+                }
+                Err(_) => {
+                    info!("Error receiving exit signal");
+                    break;
+                }
+            }
+
+            thread::sleep(poll_interval);
+        }
+    }
+
     fn get_branch_queue_outbox_requests(&self) -> Result<(Vec<Request>, OutPoint), Error> {
         let (queue_cell, queue_cell_data) = self.get_branch_queue_outbox_cell()?;
         if queue_cell_data.outbox().is_empty() {
--- ckb-bin/src/subcommand/aggregator.rs
+++ ckb-bin/src/subcommand/aggregator.rs
@@ -14,7 +14,8 @@ pub fn aggregator(args: AggregatorArgs, chain_id: String) -> Result<(), ExitCode
     let aggregator = Aggregator::new(args.config, Duration::from_secs(2), chain_id);
 
     let stop_rx_rgbpp = new_crossbeam_exit_rx();
-    let stop_rx_branch = new_crossbeam_exit_rx();
+    let stop_rx_branch_to_rgbpp_main = new_crossbeam_exit_rx();
+    let stop_rx_branch_to_rgbpp_unlock = new_crossbeam_exit_rx();
 
     const THREAD_NAME_RGBPP: &str = "Aggregator_RGBPP";
     let aggregator_rgbpp = aggregator.clone();
@@ -26,17 +27,29 @@ pub fn aggregator(args: AggregatorArgs, chain_id: String) -> Result<(), ExitCode
         .expect("Start aggregator failed!");
     register_thread(THREAD_NAME_RGBPP, aggregator_rgbpp_jh);
 
-    const THREAD_NAME_BRANCH: &str = "Aggregator_Branch";
+    const THREAD_NAME_BRANCH_1: &str = "Aggregator_Branch_to_RGBPP_Main";
     let aggregator_branch = aggregator.clone();
-    let aggregator_branch_jh = thread::Builder::new()
-        .name(THREAD_NAME_BRANCH.into())
+    let aggregator_branch_jh_1 = thread::Builder::new()
+        .name(THREAD_NAME_BRANCH_1.into())
         .spawn({
             move || {
-                aggregator_branch.poll_branch_requests(stop_rx_branch);
+                aggregator_branch.poll_branch_requests(stop_rx_branch_to_rgbpp_main);
             }
         })
         .expect("Start Branch aggregator failed!");
-    register_thread(THREAD_NAME_BRANCH, aggregator_branch_jh);
+    register_thread(THREAD_NAME_BRANCH_1, aggregator_branch_jh_1);
+
+    const THREAD_NAME_BRANCH_2: &str = "Aggregator_Branch_to_RGBPP_Unlock";
+    let aggregator_branch = aggregator.clone();
+    let aggregator_branch_jh_2 = thread::Builder::new()
+        .name(THREAD_NAME_BRANCH_2.into())
+        .spawn({
+            move || {
+                aggregator_branch.poll_rgbpp_unlock(stop_rx_branch_to_rgbpp_unlock);
+            }
+        })
+        .expect("Start Branch aggregator failed!");
+    register_thread(THREAD_NAME_BRANCH_2, aggregator_branch_jh_2);
 
     ctrlc::set_handler(|| {
         info!("Trapped exit signal, exiting...");
